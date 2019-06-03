c8886d769807ccfdfc5024dd7edea484dd80ad82
ankur-b <barveankur29@outlook.com>
2019-06-03 09:41:05 +0530

    fix footer issue

---
+++ precache-manifest.d8fd55c38a61fbef5c42f13e1b124ada.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2441d1ea30f7d92f417e6c32541e4f93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18d865737318c0378bd8",
+    "url": "/static/css/main.97c2afb2.chunk.css"
+  },
+  {
+    "revision": "d896849d2c57a5111e05",
+    "url": "/static/js/2.7d77212e.chunk.js"
+  },
+  {
+    "revision": "18d865737318c0378bd8",
+    "url": "/static/js/main.60b498f1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.83e25d1c26e176e50c099ff8553c2fd8.js"
+  "/precache-manifest.d8fd55c38a61fbef5c42f13e1b124ada.js"
 );
 
 self.addEventListener('message', (event) => {
