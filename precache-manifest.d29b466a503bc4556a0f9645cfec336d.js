844aeedba80303133b79557a2474ab1d8ccad9e1
ankur-b <barveankur29@outlook.com>
2019-12-19 16:11:18 +0530

    update template

---
+++ precache-manifest.d29b466a503bc4556a0f9645cfec336d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a14d758a5e729d0465872fb18f2cf9c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f39336aece626e75598",
+    "url": "/static/css/main.9ce039ac.chunk.css"
+  },
+  {
+    "revision": "6f316b3a315a2d77320d",
+    "url": "/static/js/2.d156eb27.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.d156eb27.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3f39336aece626e75598",
+    "url": "/static/js/main.a290d319.chunk.js"
+  },
+  {
+    "revision": "8a8d5438eb46fd8e8aef",
+    "url": "/static/js/runtime-main.e9c6b32b.js"
+  }
+]);
