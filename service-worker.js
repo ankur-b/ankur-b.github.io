c8c9b1eac4c293378da56a8a00c11623dab5fbcf
ankur-b <barveankur29@outlook.com>
2021-03-10 18:46:22 +0530

    google form implemented

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a6dc09b7aa04e75afa9eb8120c67c423.js"
+  "/precache-manifest.bc05e715d0611b95b989a5f3265841a1.js"
 );
 
 self.addEventListener('message', (event) => {
