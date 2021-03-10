a7acc431555f8657d07c51ee2f4c4cbdb2408467
ankur-b <barveankur29@outlook.com>
2021-03-10 15:06:40 +0530

    add contact blog and portfolio page

---
+++ precache-manifest.79352d03a1aad7f730c3c4eaaa918324.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b3bcd11865ce4e586c259ff4c597ba6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26a82a7e7d907388c7d0",
+    "url": "/static/css/main.fcaef4af.chunk.css"
+  },
+  {
+    "revision": "45a6a13403bee0a6aae3",
+    "url": "/static/js/2.55153b0a.chunk.js"
+  },
+  {
+    "revision": "9480b179624caedc1e29563c2c959c72",
+    "url": "/static/js/2.55153b0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26a82a7e7d907388c7d0",
+    "url": "/static/js/main.2975737f.chunk.js"
+  },
+  {
+    "revision": "deaf17d855cbdaf7344d",
+    "url": "/static/js/runtime-main.47788c5d.js"
+  },
+  {
+    "revision": "c60f54a25be48955be11a0ad03d21839",
+    "url": "/static/media/avatar.c60f54a2.jpg"
+  },
+  {
+    "revision": "4575c40bfb8dec6713d2da51c4c4066e",
+    "url": "/static/media/mariya-georgieva.4575c40b.jpg"
+  }
+]);
